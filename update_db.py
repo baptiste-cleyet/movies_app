6800948629eee2e1933c378797c2a65aa0831b6c
Baptiste <baptiste.cleyet@etu.univ-lyon1.fr>
2026-03-25 19:02:29 +0100

    README and minor changes

--- update_db.py
+++ update_db.py
@@ -5,8 +5,7 @@ connection = sqlite3.connect('database.db')
 cur = connection.cursor()
 
 cur.execute('''
-    ALTER TABLE movies
-    drop column tmdb_link;
+    # Write you SQL statement here.
 ''')
 
 connection.commit()
